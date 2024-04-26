6a2f760acc461b52afe11fab4a87cedda11e9222
Yur3te <Yur3te@gmail.com>
2024-04-26 13:20:45 +0200

    Added total calories count

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react'
 import AddMealForm from './AddMealForm';
+import BMRCalculations from './BMRCalculations'
 
 
 function App() {
   const [data, setData] = useState([])
   const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]); // Default to today
+  const [totalCalories, setTotalCalories] = useState([])
 
   
   useEffect(() => {
@@ -12,12 +14,27 @@ function App() {
     // fetch('http://localhost:8081/meals')
     fetch(`http://localhost:8081/meals?meal_date=${selectedDate}`)
     .then(response => response.json())
-
     .then(data => {
       setData(data);
       console.log(data);
     })
     .catch(err => console.log(err));
+
+
+  }, [selectedDate])
+
+  useEffect(() => {
+    
+    fetch(`http://localhost:8081/totalcalories?meal_date=${selectedDate}`)
+    .then(response => response.json())
+    .then(data => {
+      setTotalCalories(data[0].total_calories);
+      console.log(data);
+      // console.log(data[0].total_calories);
+
+    })
+    .catch(err => console.log(err));
+
   }, [selectedDate])
 
   const handleAddMeal = (mealData) => {
@@ -54,9 +71,33 @@ function App() {
         value={selectedDate}
         onChange={(e) => setSelectedDate(e.target.value)}
         />
-        {data.map((item, index) => (
+        {/* {data.map((item, index) => (
         <div key={index}>{item.meal_name}, {item.calories}</div>
-      ))}
+      ))} */}
+
+      <table>
+        <thead>
+          <th>Name</th>
+          <th>Calories</th>
+        </thead>
+        <tbody>
+        {data.map((item, index) => (
+          <tr key={index}>
+            <td>{item.meal_name}</td>
+            <td>{item.calories}</td>
+          </tr>
+        ))}
+        </tbody>
+        
+      </table>
+      
+      <div className="">
+      <p></p>
+        <h3>Today, You have already eaten : <b>{totalCalories} </b> kcal</h3>
+        {/* <br/> */}
+        <BMRCalculations/>
+        <br />
+      </div>
       </div>
   )
 }
