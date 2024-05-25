3764264f0ae25f5edf2abf5faf54c1b2d6154050
Yur3te <Yur3te@gmail.com>
2024-05-25 21:25:41 +0200

    Moved code structure to make it more clear

--- frontend/src/components/AddMealForm.js
+++ frontend/src/components/AddMealForm.js
@@ -2,27 +2,42 @@ import React, { useState } from 'react';
 
 import "./../style/AddMealForm.css"
 
-function AddMealForm({ onAddMeal }) {
+function AddMealForm() {
   const [meal_name, setMealName] = useState('');
   const [calories, setCalories] = useState('');
   const [proteins, setProteins] = useState('');
-
   const [meal_date, setMeal_date] = useState(new Date().toISOString().split('T')[0]); // Default to today
 
-
   const handleSubmit = (e) => {
+      fetch("http://localhost:8081/eaten", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ meal_name, calories, meal_date, proteins}),
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log("Meal added successfully");
+          } else {
+            throw new Error("Failed to add meal");
+          }
+        })
+        .catch((error) => console.error("Error:", error));
     e.preventDefault();
 
     // const meal_date = new Date().toISOString().split('T')[0];
 
-    onAddMeal({ meal_name, calories, meal_date, proteins});
+    // onAddMeal({ meal_name, calories, meal_date, proteins});
+    
     setMealName('');
     setCalories('');
     setProteins('');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div className="meal-form">
+      <form onSubmit={handleSubmit}>
       <input
         type="text"
         value={meal_name}
@@ -57,6 +72,7 @@ function AddMealForm({ onAddMeal }) {
 
       <button type="submit">Add Meal!</button>
     </form>
+    </div>
   );
 }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,28 +4,11 @@ import AddMealForm from "./../components/AddMealForm";
 import AllMealsInDay from "./../components/AllMealsInDay";
 
 function Home() {
-  const handleAddMeal = (mealData) => {
-    fetch("http://localhost:8081/eaten", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(mealData),
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log("Meal added successfully");
-        } else {
-          throw new Error("Failed to add meal");
-        }
-      })
-      .catch((error) => console.error("Error:", error));
-  };
 
   return (
     <div>
         <div className="add-meal">
-            <AddMealForm onAddMeal={handleAddMeal} />
+            <AddMealForm/>
         </div>
     
       <div className="add-meal">
